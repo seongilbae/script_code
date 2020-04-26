850a2ed01f303f2f2b60101a34720c553bc16d61
Sib <beasongil@hanmail.net>
2020-04-26 21:16:53 +0900

    Update requestchap.py

--- requestchap.py
+++ requestchap.py
@@ -3,9 +3,9 @@ import sys
 import urllib.request
 count=0
 while(1):
-    client_id = "pUJyqkB3NPxdc0SARfdC" # 개발자센터에서 발급받은 Client ID 값
-    client_secret = "mSRtUxNhQk" # 개발자센터에서 발급받은 Client Secret 값
-    key = "bxDQhoiBTOiQhjxJ" # 캡차 Key 값
+    client_id = "" # 개발자센터에서 발급받은 Client ID 값
+    client_secret = "" # 개발자센터에서 발급받은 Client Secret 값
+    key = "" # 캡차 Key 값 , chapkey.py output
     url = "https://openapi.naver.com/v1/captcha/ncaptcha.bin?key=" + key
     request = urllib.request.Request(url)
     request.add_header("X-Naver-Client-Id",client_id)
@@ -13,7 +13,7 @@ while(1):
     response = urllib.request.urlopen(request)
     rescode = response.getcode()
     if(rescode==200):
-        print("캡차 이미지 저장")
+        #print("캡차 이미지 저장")
         response_body = response.read()
         with open('E:\\naver_captchar_image\\captcha%d.jpg'%count, 'wb') as f:
             f.write(response_body)
